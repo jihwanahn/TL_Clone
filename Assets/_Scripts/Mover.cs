d4f2e5deb2f79713dfb153634136b6c4bcbb52dc
Jihwan <coldstew@gmail.com>
2020-02-11 15:09:09 +0900

    Assets added

---
+++ Assets/_Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    //private Ray lastRay;
+    
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //}    
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.white);
+        GetComponent<NavMeshAgent>().destination = target.position;
+        GetComponent<NavMeshAgent>().stoppingDistance = 5f;
+    }
+
+}
