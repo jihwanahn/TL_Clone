ebfad65173b11cfc467a6ac6c2639a1a8ea567f7
coldstew <coldstew@gmail.com>
2020-02-13 16:17:51 +0900

    애니메이션 다시 추가

---
+++ Assets/_Scripts/Core/ActionScheduler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58a3f1b03be0eca367538f4fc7894564eaca49d56dce2cc730cd2c8be9877e07
+size 604
--- Assets/_Scripts/Movement/Mover.cs
+++ Assets/_Scripts/Movement/Mover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2095644c9f9d7db4d0210984e98f309b4ad99790c542c09dff01bdd143b79668
-size 1217
+oid sha256:4357bae17d7fff97a265c767a00e02f75483e135e6cfaf4da81e94f4a96ef162
+size 1296
