65102c99bd9f1db27d36823207895ce86b3c1956
coldstew <coldstew@gmail.com>
2020-02-13 23:51:34 +0900

    공격 모션 로직

--- Assets/_Scripts/Combat/Fighter.cs
+++ Assets/_Scripts/Combat/Fighter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a86d30c56b3a5cbfe0a70f2655fc07a1279211105bef88985e1a7ffd30fe29b
-size 1081
+oid sha256:fb7245d5ed29dad77d18b8f526336fa9d329808f3596cac34a37f0b40dc13c60
+size 1339
