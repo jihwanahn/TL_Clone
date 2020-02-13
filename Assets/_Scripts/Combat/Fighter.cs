2ad0baf2f12d8ef2df7d8d0a38b56d226043bba9
coldstew <coldstew@gmail.com>
2020-02-13 17:40:16 +0900

    플레이어 공격 애니메이션, 코드 중

--- Assets/_Scripts/Combat/Fighter.cs
+++ Assets/_Scripts/Combat/Fighter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c652757cff459fd4482323adb4cd42e8a10c1f3a260b8a41079e1913e4e1f8b
-size 997
+oid sha256:3a86d30c56b3a5cbfe0a70f2655fc07a1279211105bef88985e1a7ffd30fe29b
+size 1081
